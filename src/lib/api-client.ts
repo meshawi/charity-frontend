86938126f3fb7f5e5c370bc9ae42f94f1a0c85ae
Mohammed Aleshawi <mhmdaleshawi@gmail.com>
2026-03-21 10:28:37 +0300

    npx tsc --noEmit

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -6,14 +6,16 @@ type RequestOptions = Omit<RequestInit, "body"> & {
 }
 
 export class ApiError extends Error {
-  constructor(
-    public status: number,
-    public code: string,
-    message: string,
-    public details?: unknown[]
-  ) {
+  status: number
+  code: string
+  details?: unknown[]
+
+  constructor(status: number, code: string, message: string, details?: unknown[]) {
     super(message)
     this.name = "ApiError"
+    this.status = status
+    this.code = code
+    this.details = details
   }
 }
 
